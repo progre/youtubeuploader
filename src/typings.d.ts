a064deb0827b599d767173f7aec14bb1c4f35c03
progre <djyayutto@gmail.com>
2017-02-19 10:56:53 +0900

    Remove old build file

--- src/typings.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
-/// <reference path="../typings/DefinitelyTyped/node/node.d.ts" />
-/// <reference path="../typings/DefinitelyTyped/chokidar/chokidar.d.ts" />
-/// <reference path="../typings/DefinitelyTyped/lockfile/lockfile.d.ts" />
-/// <reference path="../typings/DefinitelyTyped/log4js/log4js.d.ts" />
-/// <reference path="../typings/DefinitelyTyped/node-notifier/node-notifier.d.ts" />
-/// <reference path="../typings/DefinitelyTyped/node-uuid/node-uuid.d.ts" />
